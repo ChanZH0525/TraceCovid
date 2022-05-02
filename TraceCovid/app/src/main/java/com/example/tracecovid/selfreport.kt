5bce3a48dcf9699b65692aa2d5426481a8734705
ChanZH0525 <maximusczh321@gmail.com>
2022-05-02 23:31:02 +0800

    added activity and fragment pages

---
+++ TraceCovid/app/src/main/java/com/example/tracecovid/selfreport.kt
@@ -0,0 +1,11 @@
+package com.example.tracecovid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class selfreport : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selfreport)
+    }
+}
