bcdd053b71e7c4821bece21698ef106409724e39
ChanZH0525 <maximusczh321@gmail.com>
2022-05-12 01:04:01 +0800

    added home, check in and profile fragments

--- TraceCovid/app/src/main/java/com/example/tracecovid/MainActivity.kt
+++ TraceCovid/app/src/main/java/com/example/tracecovid/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.tracecovid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val homebtn: Button= findViewById(R.id.homebtn)
+        homebtn.setOnClickListener {
+
+        }
     }
+
 }
