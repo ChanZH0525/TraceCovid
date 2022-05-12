5aa935993d57cd9e70d340df271e4445ef58b0a0
ChanZH0525 <maximusczh321@gmail.com>
2022-05-12 15:42:34 +0800

    added bottom navigation, needs to double click to work though

--- TraceCovid/app/src/main/java/com/example/tracecovid/MainActivity.kt
+++ TraceCovid/app/src/main/java/com/example/tracecovid/MainActivity.kt
@@ -5,18 +5,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fragment: Fragment
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer,homepageFragment()).commit()
+
+        var bottom_navigation:BottomNavigationView =findViewById(R.id.bottom_navigation)
+        bottom_navigation.setOnNavigationItemReselectedListener(navlistener)
 
 
-        val homebtn: Button= findViewById(R.id.homebtn)
-        homebtn.setOnClickListener {
+    }
+    val navlistener =BottomNavigationView.OnNavigationItemReselectedListener {
+        when(it.itemId)
+        {
+            R.id.homepage->fragment=homepageFragment()
+            R.id.checkin->fragment=checkinFragment()
+            R.id.Hotspot->fragment=hotspotFragment()
+            R.id.profile->fragment=profileFragment()
 
         }
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer,fragment).commit()
+        true
     }
 
 }
