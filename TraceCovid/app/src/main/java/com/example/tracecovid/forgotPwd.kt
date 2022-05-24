44bd1792936801ee7207d03f9542c65899d891ff
Yyin <yinyinteo0720@gmail.com>
2022-05-24 14:29:50 +0800

    Add Forgot Password Page

---
+++ TraceCovid/app/src/main/java/com/example/tracecovid/forgotPwd.kt
@@ -0,0 +1,11 @@
+package com.example.tracecovid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class forgotPwd : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgotpwd)
+    }
+}
