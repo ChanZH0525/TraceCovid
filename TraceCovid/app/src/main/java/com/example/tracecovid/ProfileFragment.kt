0687b182f2294811c7842345a9b787800c2e4486
beh616 <84115414+beh616@users.noreply.github.com>
2022-05-25 16:23:12 +0800

    Complete ProfileFragment design

--- TraceCovid/app/src/main/java/com/example/tracecovid/ProfileFragment.kt
+++ TraceCovid/app/src/main/java/com/example/tracecovid/ProfileFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import de.hdodenhof.circleimageview.CircleImageView
 
 
 class ProfileFragment : Fragment() {
@@ -13,6 +16,19 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+//        handle for profile information
+        val profileImage: CircleImageView = view.findViewById(R.id.iv_profile)
+        val btnSetting: ImageView = view.findViewById(R.id.btn_setting_profile)
+        val tvUsername: TextView = view.findViewById(R.id.tv_username)
+        val tvPhone: TextView = view.findViewById(R.id.tv_phone)
+        val tvIC: TextView = view.findViewById(R.id.tv_ic_passport)
+        val tvState: TextView = view.findViewById(R.id.tv_state)
+
+
+
+
+        return view
     }
 }
