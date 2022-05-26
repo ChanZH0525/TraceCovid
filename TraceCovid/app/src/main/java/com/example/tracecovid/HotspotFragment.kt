5ebaef1eaeedc32d86672cbaf018b7155b593041
ChanZH0525 <maximusczh321@gmail.com>
2022-05-26 23:02:49 +0800

    added in Google Map API still incomplete

--- TraceCovid/app/src/main/java/com/example/tracecovid/HotspotFragment.kt
+++ TraceCovid/app/src/main/java/com/example/tracecovid/HotspotFragment.kt
@@ -1,14 +1,15 @@
 package com.example.tracecovid
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 
 class HotspotFragment : Fragment() {
-
 
 
     override fun onCreateView(
@@ -16,8 +17,17 @@ class HotspotFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hotspot, container, false)
+        val view = inflater.inflate(R.layout.fragment_hotspot, container, false)
+        val searchbtn: ImageView = view.findViewById(R.id.search_loc)
+
+        searchbtn.setOnClickListener {
+            requireActivity().run {
+                startActivity(Intent(view.context, MapsActivity::class.java))
+                finish()
+            }
+
+        }
+        return view
+
     }
-
-
 }
