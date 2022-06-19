8408c121a9e6979f8789d54ecc87225f08a3930e
ChanZH0525 <maximusczh321@gmail.com>
2022-06-19 15:30:18 +0800

    change password via email done

--- TraceCovid/app/src/main/java/com/example/tracecovid/ChangePwd.kt
+++ TraceCovid/app/src/main/java/com/example/tracecovid/ChangePwd.kt
@@ -1,5 +1,6 @@
 package com.example.tracecovid
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -28,37 +29,40 @@ class ChangePwd : BaseFragment() {
     ): View? {
         auth = FirebaseAuth.getInstance()
         uid = auth.currentUser?.uid.toString()
-        database =FirebaseDatabase.getInstance("https://tracecovid-e507a-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        database =
+            FirebaseDatabase.getInstance("https://tracecovid-e507a-default-rtdb.asia-southeast1.firebasedatabase.app/")
         dbreference = database.getReference("Users").child(uid)
 
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_change_pwd, container, false)
-        var curpwd:TextInputEditText= view.findViewById(R.id.curpwd)
-        var newpwd:TextInputEditText=view.findViewById(R.id.newpwd)
-        var newpwd2:TextInputEditText=view.findViewById(R.id.newpwd2)
-        val btnUpdate: Button=view.findViewById(R.id.updateBtn)
-     /*   btnUpdate.setOnClickListener{
-            if (newpwd != newpwd2 && newpwd!= curpwd)
-            {
-                var password= newpwd.text.toString()
-                val map= mapOf(
-                    "password" to password
-                )
-                dbreference.updateChildren(map)
+        var emailadd: TextInputEditText = view.findViewById(R.id.curemail)
+
+        val btnUpdate: Button = view.findViewById(R.id.updateBtn)
+        btnUpdate.setOnClickListener {
+            val email = emailadd.text.toString().trim()
+            if (email.isEmpty()) {
+                Toast.makeText(activity, "Fill in email address", Toast.LENGTH_SHORT).show()
+            } else {
+                auth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        requireActivity().run {
+                            startActivity(Intent(view.context, PwdUpdatedPage::class.java))
+                            finish()
+                        }
+                    } else {
+                        Toast.makeText(activity, "link was not sent", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            }
+        }//not done yet
+
+            val btnBack: ImageView = view.findViewById(R.id.btn_back)
+            btnBack.setOnClickListener {
                 parentFragmentManager.popBackStack()
             }
-            else
-            {
-                Toast.makeText(activity,"Change of password failed",Toast.LENGTH_SHORT).show()
-            }
 
-        } */ //not done yet
+            return view
 
-        val btnBack: ImageView = view.findViewById(R.id.btn_back)
-        btnBack.setOnClickListener {
-            parentFragmentManager.popBackStack()
         }
-
-        return view
     }
-}
