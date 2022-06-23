29cba4701091dba452bca30aa315a16046237c9a
ChanZH0525 <maximusczh321@gmail.com>
2022-06-23 15:31:48 +0800

    change password completed

--- TraceCovid/app/src/main/java/com/example/tracecovid/ChangePwd.kt
+++ TraceCovid/app/src/main/java/com/example/tracecovid/ChangePwd.kt
@@ -10,6 +10,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -18,44 +19,72 @@ import org.w3c.dom.Text
 
 class ChangePwd : BaseFragment() {
     override var bottomNavigationViewVisibility = View.GONE
-    lateinit var database: FirebaseDatabase
-    private lateinit var uid: String
+
     private lateinit var auth: FirebaseAuth
-    private lateinit var dbreference: DatabaseReference
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         auth = FirebaseAuth.getInstance()
-        uid = auth.currentUser?.uid.toString()
-        database =
-            FirebaseDatabase.getInstance("https://tracecovid-e507a-default-rtdb.asia-southeast1.firebasedatabase.app/")
-        dbreference = database.getReference("Users").child(uid)
-
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_change_pwd, container, false)
-        var emailadd: TextInputEditText = view.findViewById(R.id.curemail)
+        val currentPassword:TextInputEditText= view.findViewById(R.id.curpassword)
+        val newPassword:TextInputEditText=view.findViewById(R.id.newpassword)
+        val confirmPassword: TextInputEditText=view.findViewById(R.id.confirmpassword)
+
 
         val btnUpdate: Button = view.findViewById(R.id.updateBtn)
         btnUpdate.setOnClickListener {
-            val email = emailadd.text.toString().trim()
-            if (email.isEmpty()) {
-                Toast.makeText(activity, "Fill in email address", Toast.LENGTH_SHORT).show()
-            } else {
-                auth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        requireActivity().run {
-                            startActivity(Intent(view.context, PwdUpdatedPage::class.java))
-                            finish()
-                        }
-                    } else {
-                        Toast.makeText(activity, "link was not sent", Toast.LENGTH_SHORT).show()
+           if(currentPassword.text?.isNotEmpty()==true && newPassword.text?.isNotEmpty()==true
+               && confirmPassword.text?.isNotEmpty()==true){
+                if (newPassword.text.toString().equals(confirmPassword.text.toString()))
+                {
+                    val user=auth.currentUser
+                    if(user !=null && user.email!=null)
+                    {
+                        val credential = EmailAuthProvider
+                            .getCredential(user.email!!, currentPassword.text.toString())
+
+                        user.reauthenticate(credential)
+                            .addOnCompleteListener {
+                                if (it.isSuccessful)
+                                {
+                                    Toast.makeText(activity,"Reauthentication Succesful",Toast.LENGTH_SHORT).show()
+
+                                    user!!.updatePassword(newPassword.text.toString())
+                                        .addOnCompleteListener { task ->
+                                            if (task.isSuccessful) {
+                                                Toast.makeText(activity,"Password Changed Successfully",Toast.LENGTH_SHORT).show()
+                                                auth.signOut()
+                                                startActivity(Intent(view.context, Login::class.java))
+
+                                            }
+                                        }
+                                }
+                                else
+                                {
+                                    Toast.makeText(activity,"Reauthentication failed",Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                    }
+                    else
+                    {
+                        startActivity(Intent(view.context, Login::class.java))
+
                     }
                 }
-
+               else
+                {
+                    Toast.makeText(activity,"New password does not match",Toast.LENGTH_SHORT).show()
+                }
+           }
+            else{
+                Toast.makeText(activity,"Fill in all fields",Toast.LENGTH_SHORT).show()
             }
-        }//not done yet
+
+        }
 
             val btnBack: ImageView = view.findViewById(R.id.btn_back)
             btnBack.setOnClickListener {
