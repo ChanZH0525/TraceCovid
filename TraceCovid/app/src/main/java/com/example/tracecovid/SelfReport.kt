8768b6208a3cc9c7ed727c9495cb7b647b6724bb
YinYin0720 <SWE1904136@xmu.edu.my>
2022-06-25 04:39:15 +0800

    redo SelfReport xml

--- TraceCovid/app/src/main/java/com/example/tracecovid/SelfReport.kt
+++ TraceCovid/app/src/main/java/com/example/tracecovid/SelfReport.kt
@@ -1,11 +1,15 @@
 package com.example.tracecovid
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class SelfReport : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,21 +21,71 @@ class SelfReport : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
-        var questionnaire2: ArrayList<SelfReportClass> = arrayListOf(
-            SelfReportClass("Where was the Swab test done?"),
-            SelfReportClass("What date was the test taken?"),
-            SelfReportClass("State the outcome of your test."),
-            SelfReportClass("Where are you currently staying?"),
-            SelfReportClass("Please state your state."),
-            SelfReportClass("Please state your current postcode."),
-            )
-        val recyclerview : RecyclerView = findViewById<RecyclerView>(R.id.selfreport_recyclerview)
-        recyclerview.layoutManager = LinearLayoutManager(
-            this,
-            LinearLayoutManager.VERTICAL,
-            false
-        )
-        recyclerview.adapter = SelfReportAdapter(questionnaire2)
+
+        val cancelBtn: Button = findViewById(R.id.cancel_btn)
+        cancelBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        var date = findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.self_report_ans2)
+        DatePickerUniversal(date, "dd-MMM-yyyy")
+
+    }
+
+    override fun onResume(){
+        super.onResume()
+
+        val dropdown_swabLocation = findViewById<AutoCompleteTextView>(R.id.dropdown_swabLocation)
+        val swabLocation = resources.getStringArray(R.array.swabLocation)
+        val arrayAdapter_swabLocation = ArrayAdapter(this, R.layout.dropdown_list, swabLocation)
+        dropdown_swabLocation.setAdapter(arrayAdapter_swabLocation)
+
+        val dropdown_swabOutcome = findViewById<AutoCompleteTextView>(R.id.dropdown_swabOutcome)
+        val swabOutcome = resources.getStringArray(R.array.swabOutcome)
+        val arrayAdapter_swapOutcome = ArrayAdapter(this, R.layout.dropdown_list, swabOutcome)
+        dropdown_swabOutcome.setAdapter(arrayAdapter_swapOutcome)
+
+        val dropdown_states = findViewById<AutoCompleteTextView>(R.id.dropdown_states)
+        val state = resources.getStringArray(R.array.states)
+        val arrayAdapter_states = ArrayAdapter(this, R.layout.dropdown_list, state)
+        dropdown_states.setAdapter(arrayAdapter_states)
+    }
+
+    class DatePickerUniversal(private val mEditText: EditText, format: String?) :
+        View.OnFocusChangeListener, OnDateSetListener, View.OnClickListener {
+        private var mCalendar: Calendar? = null
+        private val mFormat: SimpleDateFormat
+        override fun onFocusChange(view: View, hasFocus: Boolean) {
+            if (hasFocus) {
+                showPicker(view)
+            }
+        }
+
+        override fun onClick(view: View) {
+            showPicker(view)
+        }
+
+        private fun showPicker(view: View) {
+            if (mCalendar == null) mCalendar = Calendar.getInstance()
+            val day: Int = mCalendar!!.get(Calendar.DAY_OF_MONTH)
+            val month: Int = mCalendar!!.get(Calendar.MONTH)
+            val year: Int = mCalendar!!.get(Calendar.YEAR)
+            DatePickerDialog(view.getContext(), this, year, month, day).show()
+        }
+
+        override fun onDateSet(view: DatePicker, year: Int, month: Int, dayOfMonth: Int) {
+            mCalendar!!.set(Calendar.YEAR, year)
+            mCalendar!!.set(Calendar.MONTH, month)
+            mCalendar!!.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            mEditText!!.setText(mFormat.format(mCalendar!!.getTime()))
+        }
+
+        init {
+            mEditText.setOnFocusChangeListener(this)
+            mEditText.setOnClickListener(this)
+            mFormat = SimpleDateFormat(format, Locale.getDefault())
+        }
     }
 
 }
--- TraceCovid/app/src/main/java/com/example/tracecovid/SelfReportClass.kt
+++
@@ -1,7 +0,0 @@
-package com.example.tracecovid
-
-
-data class SelfReportClass(
-    var question:String,
-  //  var answer: String
-)
