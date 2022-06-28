0a3caa508e8ddbcc2da692b5c789a6cfe1f8fdd7
ChanZH0525 <maximusczh321@gmail.com>
2022-06-29 00:49:50 +0800

    hotspot 50% done

--- TraceCovid/app/src/main/java/com/example/tracecovid/HotspotFragment.kt
+++ TraceCovid/app/src/main/java/com/example/tracecovid/HotspotFragment.kt
@@ -1,64 +1,75 @@
 package com.example.tracecovid
 
-import android.content.Intent
+//import android.widget.SearchView
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-//import android.widget.SearchView
-import androidx.appcompat.widget.SearchView
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentResultListener
-import androidx.fragment.app.findFragment
 import com.example.tracecovid.databinding.FragmentHotspotBinding
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
 
-class HotspotFragment : BaseFragment() {
+class HotspotFragment : BaseFragment(), OnMapReadyCallback {
     override var bottomNavigationViewVisibility = View.VISIBLE
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var dbreference: DatabaseReference
     private  lateinit var firebaseDB: FirebaseDatabase
-    private lateinit var binding: FragmentHotspotBinding
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_hotspot, container, false)
         firebaseAuth = FirebaseAuth.getInstance()
-        firebaseDB= FirebaseDatabase.getInstance("https://tracecovid-e507a-default-rtdb.asia-southeast1.firebasedatabase.app/")
-        dbreference=firebaseDB.getReference("Hotspot")
-        binding = FragmentHotspotBinding.inflate(inflater, container, false)
-        var cases:HotpotData
+        firebaseDB =
+            FirebaseDatabase.getInstance("https://tracecovid-e507a-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        dbreference = firebaseDB.getReference("Hotspot")
+
+        var cases: HotpotData
+        val dropdown_states = view.findViewById<AutoCompleteTextView>(R.id.dropdown_states)
+        //Google Map
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        val state = resources.getStringArray(R.array.states)
+        val arrayAdapter_states = view?.let { ArrayAdapter(it.context, R.layout.dropdown_list, state) }
+        dropdown_states.setAdapter(arrayAdapter_states)
 
 
-        binding.searchBtn.setOnClickListener{
-            dbreference.child(binding.idSearchView.text.toString()).addValueEventListener(object :ValueEventListener{
+
+        /*binding.searchBtn.setOnClickListener{
+        dbreference.child(binding.idSearchView.text.toString())
+            .addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
-                    if (binding.idSearchView.text.toString()!="Selangor" &&
-                        binding.idSearchView.text.toString()!="Johor" &&
-                        binding.idSearchView.text.toString()!="Kuala Lumpur"&&
-                        binding.idSearchView.text.toString()!="Terrenganu" &&
-                        binding.idSearchView.text.toString()!="Malacca" &&
-                        binding.idSearchView.text.toString()!="Negeri Sembilan" &&
-                        binding.idSearchView.text.toString()!="Kedah" &&
-                        binding.idSearchView.text.toString()!="Kelantan" &&
-                        binding.idSearchView.text.toString()!="Labuan" &&
-                        binding.idSearchView.text.toString()!="Pahang" &&
-                        binding.idSearchView.text.toString()!="Perlis" &&
-                        binding.idSearchView.text.toString()!="Sabah" &&
-                        binding.idSearchView.text.toString()!="Sarawak" &&
-                        binding.idSearchView.text.toString()!="Putrajaya")
-                    {
-                        Toast.makeText(activity,"Please find a valid state in Malaysia", Toast.LENGTH_SHORT).show()
-                    }
-                    else {
-                        cases = snapshot.getValue(HotpotData::class.java)!! //crashes if entered search does not exist
+                    if (binding.idSearchView.text.toString() != "Selangor" &&
+                        binding.idSearchView.text.toString() != "Johor" &&
+                        binding.idSearchView.text.toString() != "Kuala Lumpur" &&
+                        binding.idSearchView.text.toString() != "Terrenganu" &&
+                        binding.idSearchView.text.toString() != "Malacca" &&
+                        binding.idSearchView.text.toString() != "Negeri Sembilan" &&
+                        binding.idSearchView.text.toString() != "Kedah" &&
+                        binding.idSearchView.text.toString() != "Kelantan" &&
+                        binding.idSearchView.text.toString() != "Labuan" &&
+                        binding.idSearchView.text.toString() != "Pahang" &&
+                        binding.idSearchView.text.toString() != "Perlis" &&
+                        binding.idSearchView.text.toString() != "Sabah" &&
+                        binding.idSearchView.text.toString() != "Sarawak" &&
+                        binding.idSearchView.text.toString() != "Putrajaya"
+                    ) {
+                        Toast.makeText(
+                            activity,
+                            "Please find a valid state in Malaysia",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        cases =
+                            snapshot.getValue(HotpotData::class.java)!! //crashes if entered search does not exist
                         binding.reportedcasesTxt.setText("There are " + cases.Cases + " reported cases of Covid-19  in " + binding.idSearchView.text.toString())
                     }
                 }
@@ -68,13 +79,21 @@ class HotspotFragment : BaseFragment() {
                 }
 
             })
-        }
+    } */
+        return view
 
+    }
 
+    override fun onMapReady( googleMap:GoogleMap) {
+        mMap = googleMap
 
+        val location = LatLng(3.1390, 101.6869) //KL
 
-        return binding.root
-
+        mMap.addMarker(MarkerOptions().position(location).title("Marker in KL"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location))
+        val center = CameraUpdateFactory.newLatLng(LatLng(location.latitude, location.longitude))
+        val zoom = CameraUpdateFactory.zoomTo(12f)
+        mMap.animateCamera(zoom)
     }
 }
 
