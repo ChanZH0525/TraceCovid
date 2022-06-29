2d11a808b6bcf3af330e2b6fa1e89bcdaa79e82b
ChanZH0525 <maximusczh321@gmail.com>
2022-06-29 23:16:49 +0800

    Current location can now be shown

--- TraceCovid/app/src/main/java/com/example/tracecovid/HotspotFragment.kt
+++ TraceCovid/app/src/main/java/com/example/tracecovid/HotspotFragment.kt
@@ -1,26 +1,42 @@
 package com.example.tracecovid
 
 //import android.widget.SearchView
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.core.app.ActivityCompat
 import com.example.tracecovid.databinding.FragmentHotspotBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import org.w3c.dom.Text
+import java.io.IOException
 
 
-class HotspotFragment : BaseFragment(), OnMapReadyCallback {
+class HotspotFragment : BaseFragment(), OnMapReadyCallback,    GoogleMap.OnMarkerClickListener {
     override var bottomNavigationViewVisibility = View.VISIBLE
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var dbreference: DatabaseReference
     private  lateinit var firebaseDB: FirebaseDatabase
-
-    private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+    private lateinit var map: GoogleMap
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,67 +50,106 @@ class HotspotFragment : BaseFragment(), OnMapReadyCallback {
 
         var cases: HotpotData
         val dropdown_states = view.findViewById<AutoCompleteTextView>(R.id.dropdown_states)
-        //Google Map
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        val reportedCases:TextView= view.findViewById(R.id.reportedcases_txt)
+        val searchBtn:Button=view.findViewById(R.id.search_btn)
+
         val state = resources.getStringArray(R.array.states)
         val arrayAdapter_states = view?.let { ArrayAdapter(it.context, R.layout.dropdown_list, state) }
         dropdown_states.setAdapter(arrayAdapter_states)
+        //show cases in searched state
+        searchBtn.setOnClickListener{
+            dbreference.child(dropdown_states.text.toString())
+                .addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        cases = snapshot.getValue(HotpotData::class.java)!! //crashes if entered search does not exist
+                        reportedCases.setText("There are " + cases.Cases + " reported cases of Covid-19  in " + dropdown_states.text.toString())
 
-
-
-        /*binding.searchBtn.setOnClickListener{
-        dbreference.child(binding.idSearchView.text.toString())
-            .addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    if (binding.idSearchView.text.toString() != "Selangor" &&
-                        binding.idSearchView.text.toString() != "Johor" &&
-                        binding.idSearchView.text.toString() != "Kuala Lumpur" &&
-                        binding.idSearchView.text.toString() != "Terrenganu" &&
-                        binding.idSearchView.text.toString() != "Malacca" &&
-                        binding.idSearchView.text.toString() != "Negeri Sembilan" &&
-                        binding.idSearchView.text.toString() != "Kedah" &&
-                        binding.idSearchView.text.toString() != "Kelantan" &&
-                        binding.idSearchView.text.toString() != "Labuan" &&
-                        binding.idSearchView.text.toString() != "Pahang" &&
-                        binding.idSearchView.text.toString() != "Perlis" &&
-                        binding.idSearchView.text.toString() != "Sabah" &&
-                        binding.idSearchView.text.toString() != "Sarawak" &&
-                        binding.idSearchView.text.toString() != "Putrajaya"
-                    ) {
-                        Toast.makeText(
-                            activity,
-                            "Please find a valid state in Malaysia",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    } else {
-                        cases =
-                            snapshot.getValue(HotpotData::class.java)!! //crashes if entered search does not exist
-                        binding.reportedcasesTxt.setText("There are " + cases.Cases + " reported cases of Covid-19  in " + binding.idSearchView.text.toString())
                     }
-                }
+                    override fun onCancelled(error: DatabaseError) {
 
-                override fun onCancelled(error: DatabaseError) {
+                    }
+                })
+        }
 
-                }
+        //Google Map
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity().applicationContext)
 
-            })
-    } */
+
+
+
         return view
 
     }
 
     override fun onMapReady( googleMap:GoogleMap) {
-        mMap = googleMap
+        map = googleMap
 
-        val location = LatLng(3.1390, 101.6869) //KL
+        map = googleMap
 
-        mMap.addMarker(MarkerOptions().position(location).title("Marker in KL"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(location))
-        val center = CameraUpdateFactory.newLatLng(LatLng(location.latitude, location.longitude))
-        val zoom = CameraUpdateFactory.zoomTo(12f)
-        mMap.animateCamera(zoom)
+        map.uiSettings.isZoomControlsEnabled = true
+        map.setOnMarkerClickListener(this)
+        setUpMap()
     }
+
+    override fun onMarkerClick(p0: Marker): Boolean {
+        return false
+    }
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(requireActivity(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(),
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+
+        map.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(requireActivity()) { location ->
+            // Got last known location. In some rare situations this can be null.
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                placeMarkerOnMap(currentLatLng)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+            }
+        }
+    }
+
+    private fun placeMarkerOnMap(location: LatLng) {
+        val markerOptions = MarkerOptions().position(location)
+
+        val titleStr = getAddress(location)  // add these two lines
+        markerOptions.title(titleStr)
+
+        map.addMarker(markerOptions)
+    }
+
+    private fun getAddress(latLng: LatLng): String {
+        // 1
+        val geocoder = Geocoder(activity)
+        val addresses: List<Address>?
+        val address: Address?
+        var addressText = ""
+
+        try {
+            // 2
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+            // 3
+            if (null != addresses && !addresses.isEmpty()) {
+                address = addresses[0]
+                for (i in 0 until address.maxAddressLineIndex) {
+                    addressText += if (i == 0) address.getAddressLine(i) else "\n" + address.getAddressLine(i)
+                }
+            }
+        } catch (e: IOException) {
+            Log.e("MapsActivity", e.localizedMessage)
+        }
+
+        return addressText
+    }
+
 }
 
 
